f85545ce2004e9e6dbaf0e85500c7f279857abca
Anitha Rani <anitharaniganesh01@gmail.com>
2025-10-24 23:34:32 +0530

    Use environment variable for password (secure)

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import getpass
+import os
 
 def secure_login():
     password = getpass.getpass("Enter admin password: ")
-    stored_password = "UseENVorHash123"  # Example: use env variable or hashed password
+    stored_password = os.environ.get("ADMIN_PASSWORD")  # Get password from environment
     if password == stored_password:
         print("Access granted!")
     else:
